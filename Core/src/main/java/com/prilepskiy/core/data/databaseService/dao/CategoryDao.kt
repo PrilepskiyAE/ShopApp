2432fb6cfa1300afe140fa6451e5d76a2e9e822b
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-06-16 03:51:36 +0700

    bugFix database

--- Core/src/main/java/com/prilepskiy/core/data/databaseService/dao/CategoryDao.kt
+++ Core/src/main/java/com/prilepskiy/core/data/databaseService/dao/CategoryDao.kt
@@ -1,6 +1,7 @@
 package com.prilepskiy.core.data.databaseService.dao
 
+import androidx.room.Dao
 import com.prilepskiy.core.data.databaseService.entity.CategoryEntity
-
+@Dao
 abstract class CategoryDao: BaseDao<CategoryEntity>() {
 }
--- Core/src/main/java/com/prilepskiy/core/data/databaseService/dao/MealDao.kt
+++ Core/src/main/java/com/prilepskiy/core/data/databaseService/dao/MealDao.kt
@@ -1,6 +1,7 @@
 package com.prilepskiy.core.data.databaseService.dao
 
+import androidx.room.Dao
 import com.prilepskiy.core.data.databaseService.entity.MealEntity
-
+@Dao
 abstract class MealDao: BaseDao<MealEntity>() {
 }
